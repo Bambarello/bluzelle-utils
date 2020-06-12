d917a1f298ba4357eadf7db629f01f89eef00709
Sergey <52304443+c29r3@users.noreply.github.com>
2020-06-12 10:14:07 +0300

    Update unjail.sh

--- unjail.sh
+++ unjail.sh
@@ -1,1 +1,1 @@
-blzcli tx slashing unjail --from $MONIKER --node $RPC_LADDR --gas-adjustment 1.5 --gas auto --gas-prices 10.0ubnt --chain-id bluzelle -y
+$(which blzcli) tx slashing unjail --from $MONIKER --node $RPC_LADDR --gas-adjustment 1.5 --gas auto --gas-prices 10.0ubnt --chain-id bluzelle -y
